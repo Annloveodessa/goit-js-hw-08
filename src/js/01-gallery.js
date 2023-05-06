739d83929d14736e3abf735ef37c930f8e8cd3cb
Annloveodessa <anjel1112@gmail.com>
2023-05-06 14:11:28 +0200

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const imgMarkup = createImagesGallery(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createImagesGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt= "${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+const KEY = 'feedback-form-state';
+let savedData = JSON.parse(localStorage.getItem(KEY)) || {};
 
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', throttle(saveValue,500));
+formEl.addEventListener('submit', sendData);
+
+function saveValue(evt) {
+  savedData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(KEY, JSON.stringify(savedData));
+}
+if (savedData) {
+  formEl.elements.email.value = savedData.email || '';
+  formEl.elements.message.value = savedData.message || '';
+}
+function sendData(evt) {
+  evt.preventDefault();
+  if (savedData.email === '' || savedData.message === '') {
+    alert('Fill all fields');
+    return;
+  }
+  console.log(savedData);
+  localStorage.removeItem(KEY);
+  savedData = {};
+  formEl.reset();
+}
